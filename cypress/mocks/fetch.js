9c6c3a0255208767a5e8031677108e193ba46e67
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-06-23 09:55:31 -0300

    implementa testes para a tela de detalhes de comidas e bebidas

--- cypress/mocks/fetch.js
+++ cypress/mocks/fetch.js
@@ -50,7 +50,8 @@ const fetch = (url) => Promise.resolve({
 
     if (
       url === 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Aquamarine' ||
-      url === 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+      url === 'https://www.thecocktaildb.com/api/json/v1/1/random.php' ||
+      url === 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=178319'
     )
       return Promise.resolve(oneDrink);
 

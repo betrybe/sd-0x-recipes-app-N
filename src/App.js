843eb668e34b5b1b5abf146f98e31abe0594f2b5
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-12-16 10:28:13 -0200

    Fix css

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div id="meals">
-      <span>TRYBE</span>
+    <div className="meals">
+      <span className="logo">TRYBE</span>
       <object type="image/svg+xml" data={ rockGlass }> </object>
     </div>
   );
